ecada9ce3372f58846f919cffb2f38f888b8d432
saim hashmi <hashmisaim037@gmail.com>
2024-09-24 10:10:46 -0400

    animation - 6

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -11,32 +11,44 @@ const NoteItem = (props) => {
     <>
       <div className="col-md-3">
         <motion.div
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
           className="card my-3"
         >
           <div className="card-body">
             <h5 className="card-title">{note.title}</h5>
             <p className="card-text">{note.description}</p>
-            <button
-              className="btn btn-danger mx-1"
-              onClick={() => {
-                deleteNote(note._id);
-                props.showAlert("Deleted Successfully", "warning");
-              }}
+
+            {/* Button Container */}
+            <motion.div
+              initial={{ opacity: 0, y: 10 }}
+              whileHover={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.3 }}
+              className="button-container"
             >
-              <i className="fa-solid fa-trash"></i>
-            </button>
-            <button
-              className="btn btn-primary mx-1"
-              data-bs-toggle="modal"
-              data-bs-target="#myModal"
-              onClick={() => {
-                updateNote(note);
-              }}
-            >
-              <i className="fa-regular fa-pen-to-square"></i>
-            </button>
+              <motion.button
+                whileHover={{ scale: 1.2 }}
+                className="btn btn-danger mx-1"
+                onClick={() => {
+                  deleteNote(note._id);
+                  props.showAlert("Deleted Successfully", "warning");
+                }}
+              >
+                <i className="fa-solid fa-trash"></i>
+              </motion.button>
+
+              <motion.button
+                whileHover={{ scale: 1.2 }}
+                className="btn btn-primary mx-1"
+                data-bs-toggle="modal"
+                data-bs-target="#myModal"
+                onClick={() => {
+                  updateNote(note);
+                }}
+              >
+                <i className="fa-regular fa-pen-to-square"></i>
+              </motion.button>
+            </motion.div>
           </div>
         </motion.div>
       </div>
